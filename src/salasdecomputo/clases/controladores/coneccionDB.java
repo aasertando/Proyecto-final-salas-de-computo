23bc412259b541628b8334d840c9f30acbca6f01
Juan Mestra <juampames3@gmail.com>
2026-05-09 12:14:04 -0500

    subida de archivos iniciales

---
+++ src/salasdecomputo/clases/controladores/coneccionDB.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package salasdecomputo.clases.controladores;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author aser
+ */
+public class coneccionDB {
+    
+    public Connection conectarDB(){
+        try{
+            String url = "jdbc:mysql://localhost:3306/salas_computo";
+            String user = "root";
+            String pasw = "";
+            
+            Connection con = DriverManager.getConnection(url, user, pasw);
+            System.out.println("coneccion con la DB exitosa");
+            
+            return con;
+                    
+        }catch(SQLException e){
+            System.out.println("errror en la coneccion de la DB");
+            return null;
+        }
+    }
+    
+}
