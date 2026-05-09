7a0a1ed035b347a8471d9bd032b56ea27b85c8ae
Juan Mestra <juampames3@gmail.com>
2026-05-09 15:16:17 -0500

    se termina el metodo de buscar estudiante por credenciales en estudianteDAO y se elimina la coneccion de la base de datos en el main debido a que no era necesaria para nada, solo se debe conectar con la base de datos cuando se necesite

--- src/salasdecomputo/clases/estudianteDAO.java
+++ src/salasdecomputo/clases/estudianteDAO.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package salasdecomputo.clases;
+import salasdecomputo.clases.controladores.coneccionDB;
+import java.sql.*;
 
 /**
  *
@@ -10,4 +12,40 @@ package salasdecomputo.clases;
  */
 public class estudianteDAO {
     
+    //si regresa null los credenciales ingresados son invalidos
+    public static estudiante  buscarEstudiante(String usr, String pass){
+        
+
+        Connection con = coneccionDB.conectarDB();
+        
+        try{
+
+            PreparedStatement ps = con.prepareStatement("SELECT *  FROM estudiante WHERE usuario = ? AND contraseña = ?");
+            ps.setString(1, usr);
+            ps.setString(2, pass);
+
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()){
+            
+                int id = rs.getInt("idEstudiante");
+                String nombre = rs.getString("nombre");
+                String contraseña = rs.getString("contraseña");
+                boolean activo = rs.getBoolean("activo");
+                String carrera = rs.getString("carrera");
+                
+                estudiante e = new estudiante(id, nombre, contraseña, activo, carrera);
+                return e;
+                
+        }
+            
+            } catch(SQLException e){
+                System.out.println(e.getMessage());
+                System.out.println("errror\n------------------------------------");
+                System.out.println(e);
+            }
+        
+        //si no se encontró
+        return null;
+    }
 }
